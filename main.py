e405f4a80d524a6640711e00c8e7cd0b46dc3726
tokyohandsome <34906599+tokyohandsome@users.noreply.github.com>
2024-02-17 23:13:17 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,134 @@
+'''
+Supported mp3 sampling rate = 44.1KHz
+'''
+
+import flet as ft
+import os
+import re
+
+url = "https://github.com/mdn/webaudio-examples/blob/main/audio-analyser/viper.mp3?raw=true"
+audio_dir= "audio/"
+#audio_file = "Ochiai.mp3"
+#audio_file = "YoroTakeshi.m4a"
+audio_file = "YoroTakeshi.m4a"
+srt_dir = "text/"
+srt_file = os.path.splitext(os.path.basename(audio_file))[0]+".txt"
+print(srt_dir+srt_file)
+if srt_dir+srt_file:
+    print("File exists.")
+
+# Create tables from text file.
+# subs[n] = [index_number: str, start_time: str, text: str]
+def create_subtitles(file):
+    subs = []
+    sub = []
+    counter = 0
+    with open(file, 'r') as h:
+        for line in h.readlines():
+            line = line.rstrip()
+            if counter % 3 == 0:
+                sub = sub + [line]
+                counter += 1
+            elif counter % 3 == 1:
+                sub = sub + [line.zfill(8)]
+                counter += 1
+            else:
+                sub = sub + [line]
+                subs.append(sub)
+                sub = []
+                counter += 1
+    return(subs)
+
+def main(page: ft.Page):
+    page.isPlaying = False
+    page.position = 0
+    page.scroll = "adaptive"
+
+    def loaded(e):
+        print("Loaded")
+        audio_slider.max = audio1.get_duration()
+        #audio_slider.label = audio1.get_current_position()
+        print("Duration:", audio_slider.max)
+        audio_slider.divisions = audio_slider.max//60
+        subtitles = create_subtitles("assets/"+srt_dir+srt_file)
+        print(subtitles)
+        page.update()
+
+    def position_changed(e):
+        audio_slider.value = e.data
+        print("Position:", audio_slider.value)
+        page.update()
+
+    def slider_changed(e):
+        audio1.seek(int(audio_slider.value))
+        print(int(audio_slider.value))
+        page.update()
+
+    def play_button_clicked(e):
+        #print("Playing?", page.isPlaying)
+        page.position = audio1.get_current_position()
+        #print("Position:", page.position)
+        if (page.isPlaying == False) and (page.position == 0):
+            audio1.play()
+            page.isPlaying = True
+            play_button.text = "Playing"
+        elif page.isPlaying == False:
+            audio1.resume()
+            page.isPlaying = True
+            play_button.text = "Playing"
+        else:
+            audio1.pause()
+            page.isPlaying = False
+            play_button.text = "Paused"
+        page.update()
+
+    audio1 = ft.Audio(
+        src=audio_dir+audio_file,
+        #autoplay = True,
+        volume=1,
+        balance=0,
+        on_loaded=loaded,
+        on_position_changed = position_changed,
+        #on_state_changed=lambda e: isPlaying=e.data,
+        #on_state_changed=play_button_clicked,
+        on_seek_complete=lambda _: print("Seek complete"),
+    )
+    page.overlay.append(audio1)
+    
+    audio_slider = ft.Slider(
+        min = 0,
+        value = int(page.position/10000),
+        label = "{value}ms",
+        on_change = slider_changed,
+    )
+
+    play_button = ft.ElevatedButton(
+        text = "Play",
+        #on_click=play_button_clicked(isPlaying)
+        on_click=play_button_clicked
+    )
+
+    position_duration = ft.Row([
+        ft.Text(value=audio_slider.value, text_align=ft.TextAlign.LEFT),
+        ft.Text(value=audio_slider.max, text_align=ft.TextAlign.RIGHT),
+    ], #alignment=ft.MainAxisAlignment.SPACE_EVENLY,
+    )
+    
+    page.add(
+        ft.Text(value=f"Base Directories: assets/audio and assets/text"),
+        ft.Text(value=f"Audio File: {audio_file}"),
+        ft.Text(value=f"Text File: {srt_file}"),
+        audio_slider,
+        position_duration,
+        ft.Row([
+            play_button,
+            ft.ElevatedButton(
+                "Get current position",
+                on_click=lambda _: print("Current position:", audio1.get_current_position()),
+            )
+            ])
+        ),
+    
+
+ft.app(target=main, assets_dir="assets")
+#ft.app(target=main)
